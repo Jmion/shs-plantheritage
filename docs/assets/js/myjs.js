f6c71efb7880326bbb4f123a559fc581419e9a13
Jeremy Mion <jeremy.mion@epfl.ch>
2020-04-28 15:16:52 +0200

    update to website with signup page

--- docs/assets/js/myjs.js
+++ docs/assets/js/myjs.js
@@ -1,3 +1,15 @@
 if(window.location.pathname ==="/seeds.html"){
     $("#seedsMenue").addClass("active")
 }
+if(window.location.pathname ==="/map.html"){
+    $("#mapMenu").addClass("active")
+}
+if(window.location.pathname ==="/login.html"){
+    $("#loginMenu").addClass("active")
+}
+if(window.location.pathname ==="/signup.html"){
+    $("#singupMenu").addClass("active")
+}
+if(window.location.pathname ==="/experience.html"){
+    $("#experienceMenu").addClass("active")
+}
