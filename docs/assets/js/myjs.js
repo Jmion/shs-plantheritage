a92a3a527c509ec30cee7c227b56c956475519f8
Jeremy Mion <jeremy.mion@epfl.ch>
2020-04-29 00:24:37 +0200

    Added story page

--- docs/assets/js/myjs.js
+++ docs/assets/js/myjs.js
@@ -1,15 +1,21 @@
 if(window.location.pathname ==="/seeds.html"){
     $("#seedsMenue").addClass("active")
 }
-if(window.location.pathname ==="/map.html"){
+else if(window.location.pathname ==="/map.html"){
     $("#mapMenu").addClass("active")
 }
-if(window.location.pathname ==="/login.html"){
+else if(window.location.pathname ==="/login.html"){
     $("#loginMenu").addClass("active")
 }
-if(window.location.pathname ==="/signup.html"){
+else if(window.location.pathname ==="/signup.html"){
     $("#singupMenu").addClass("active")
 }
-if(window.location.pathname ==="/experience.html"){
+else if(window.location.pathname ==="/experience.html"){
     $("#experienceMenu").addClass("active")
+}
+else if(window.location.pathname ==="/experience.html"){
+    $("#experienceMenu").addClass("active")
+}
+else if(window.location.pathname ==="/story.html"){
+    $("#storyMenu").addClass("active")
 }
